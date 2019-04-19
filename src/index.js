018678d2d524b85d359a5cdc2f94783126e3f431
Sung M. Kim <sungmkim80@gmail.com>
2019-04-18 22:07:25 -0400

    The basics part 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-import Counter from "./Counter";
+import Counter from './Counter'
 
-import "./styles.css";
+import './styles.css'
+
+const log = console.log
+
+const initialState = { count: 0 }
+
+function reducer(state = initialState, action) {
+  log(`action`, action)
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 }
+    case 'DECREMENT':
+      return { count: state.count - 1 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+console.log(`done`, store.getState())
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <Counter />
-    </div>
-  );
+    </Provider>
+  )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
